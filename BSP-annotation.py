2e0748ace7079e91b1cd86b6d2aff0128e607e7c
GeneralGrube <95590493+GeneralGrube@users.noreply.github.com>
2025-09-25 10:25:43 +0200

    v3

--- BSP-annotation.py
+++ BSP-annotation.py
@@ -1,6 +1,5 @@
 import streamlit as st
-from streamlit_plotly_events import plotly_events
-import streamlit_shortcuts
+import streamlit_hotkeys as hotkeys
 from streamlit import session_state as ss
 import pandas as pd
 import numpy as np
@@ -12,6 +11,14 @@ from datetime import timedelta
 
 st.set_page_config(layout="wide")
 
+annotation_dict = {
+    #"Artifact - Missing data": 0,
+    #"Artifact - Saturation": 1,
+    "Artifact - Loose channel": 2,
+    "Suppression": 3,
+    "Other Event": 4
+}
+
 if "bucket_idx" not in ss:
     ss["bucket_idx"] = 0
 if "max_bucket" not in ss:
@@ -19,9 +26,11 @@ if "max_bucket" not in ss:
 if "bucket_cnt" not in ss:
     ss["bucket_cnt"] = False
 if "annot_df" not in ss:
-    ss["annot_df"] = pd.DataFrame(data=None, columns=["begin_idx", "end_idx", "annot_id", "annot_txt", "neurologist"])
+    ss["annot_df"] = pd.DataFrame(data=None, columns=["begin_idx", "end_idx", "trace", "annot_id", "annot_txt", "neurologist", "comment"])
 if "timestamp1" not in ss: #temporäre Variablen, um den Zustand der aktuellen Annotation zu tracken.
     ss["timestamp1"] = None
+if "ts1_curve" not in ss: #temporäre Variablen, um den Zustand der aktuellen Annotation zu tracken.
+    ss["ts1_curve"] = None
 if "timestamp2" not in ss:
     ss["timestamp2"] = None
 if "view_ts" not in ss:
@@ -97,27 +106,61 @@ def get_annot():
     else:
         return st.session_state["annot_select"]
 
+hotkeys.activate([
+    hotkeys.hk("previous", "ArrowLeft"),             
+    hotkeys.hk("next", "ArrowRight")
+    ])
+
+if hotkeys.pressed("previous"):
+    dec_bucket_idx()
+
+if hotkeys.pressed("next"):
+    inc_bucket_idx()
+
+def build_overview_base(data):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=data[::20].index, y=data["EEG1"][::20]+50, mode="lines", marker=dict(color='#1f77b4')))
+    fig.add_trace(go.Scatter(x=data[::20].index, y=data["EEG2"][::20]-50, mode="lines", marker=dict(color='#2ca02c')))
+    fig.update_layout(height=150, showlegend=False, yaxis_range=[-200, 200], margin=dict(l=20, r=20, t=0, b=0))
+    fig["layout"]["yaxis"]["title"] = "POSITION"
+    return fig
+
+def build_overview(data, bins):
+    fig = build_overview_base(data)
+    x0 = bins[st.session_state["bucket_idx"]] - pd.to_timedelta(1, unit='s')
+    try:
+        x1 = bins[(st.session_state["bucket_idx"]+1)] + pd.to_timedelta(1, unit='s')
+    except IndexError:
+        x1 = data.index[-1]
+    fig.add_vrect(x0=x0, x1=x1, line_width=3, fillcolor="black", opacity=0.3)                 
+    return fig
+
 def build_plot(data, bins, bins_dt):
     fig = make_subplots(rows=2, cols=1, shared_xaxes=True, vertical_spacing=0)
     lower_end = bins[st.session_state["bucket_idx"]]  - pd.to_timedelta(1, unit='s')
     if st.session_state["max_bucket"] == False:
         higher_end = bins[(st.session_state["bucket_idx"]+1)] + pd.to_timedelta(1, unit='s')
-        fig.add_trace(go.Scatter(x=data[lower_end:higher_end].index, y=data[lower_end:higher_end]["EEG1"].rolling(ss["setup_dict"]["smooth"]).mean().bfill(), mode="lines", marker=dict(color='#1f77b4')), row=1, col=1)
-        fig.add_trace(go.Scatter(x=data[lower_end:higher_end].index, y=data[lower_end:higher_end]["EEG2"].rolling(ss["setup_dict"]["smooth"]).mean().bfill(), mode="lines", marker=dict(color='#2ca02c')), row=2, col=1)
+        fig.add_trace(go.Scatter(x=data[lower_end:higher_end].index, y=data[lower_end:higher_end]["EEG1"].rolling(ss["setup_dict"]["smooth"]).mean().bfill(), mode="lines+markers", marker=dict(color='#1f77b4')), row=1, col=1)
+        fig.add_trace(go.Scatter(x=data[lower_end:higher_end].index, y=data[lower_end:higher_end]["EEG2"].rolling(ss["setup_dict"]["smooth"]).mean().bfill(), mode="lines+markers", marker=dict(color='#2ca02c')), row=2, col=1)
 
     else:
-        fig.add_trace(go.Scatter(x=data[lower_end:].index, y=data[lower_end:]["EEG1"].rolling(ss["setup_dict"]["smooth"]).mean().bfill(), mode="lines", marker=dict(color='#1f77b4')), row=1, col=1)
-        fig.add_trace(go.Scatter(x=data[lower_end:].index, y=data[lower_end:]["EEG2"].rolling(ss["setup_dict"]["smooth"]).mean().bfill(), mode="lines", marker=dict(color='#2ca02c')), row=2, col=1)
+        fig.add_trace(go.Scatter(x=data[lower_end:].index, y=data[lower_end:]["EEG1"].rolling(ss["setup_dict"]["smooth"]).mean().bfill(), mode="lines+markers", marker=dict(color='#1f77b4')), row=1, col=1)
+        fig.add_trace(go.Scatter(x=data[lower_end:].index, y=data[lower_end:]["EEG2"].rolling(ss["setup_dict"]["smooth"]).mean().bfill(), mode="lines+markers", marker=dict(color='#2ca02c')), row=2, col=1)
 
+    # add annotations
     for idx, annot in st.session_state["annot_df"].iterrows():
         low = bins_dt[st.session_state["bucket_idx"]] - timedelta(seconds=1)
         if st.session_state["max_bucket"] == False:
             high = bins_dt[st.session_state["bucket_idx"]+1] + timedelta(seconds=1)
         else:
             high = data.index[-1]
-        if (annot[0] >= low) & (annot[0] <= high):
-            fig.add_vrect(x0=annot[0], x1=annot[1], line_width=0, fillcolor="red", opacity=0.2)
-            fig.add_annotation(x=annot[0], y=-20, text=annot[3], showarrow=False)
+        if (annot.iloc[0] >= low) & (annot.iloc[0] <= high):
+            if annot.iloc[2] == 3:
+                fig.add_vrect(x0=annot.iloc[0], x1=annot.iloc[1], line_width=0, fillcolor="red", opacity=0.2)
+                fig.add_annotation(x=annot.iloc[0], y=-20, text=annot.iloc[4], showarrow=False)
+            else:
+                fig.add_vrect(x0=annot.iloc[0], x1=annot.iloc[1], line_width=0, fillcolor="red", opacity=0.2, row=annot.iloc[2])
+                fig.add_annotation(x=annot.iloc[0], y=-20, text=annot.iloc[4], showarrow=False, row=annot.iloc[2], col=1)
 
     # plot currect annotation
     curr_annot = []
@@ -134,31 +177,21 @@ def build_plot(data, bins, bins_dt):
         else:
             high = data.index[-1]
         if (annot >= low) & (annot <= high):
-            fig.add_vline(x=annot)
+            fig.add_vline(x=annot, row=ss.ts1_curve+1)
 
     ylow, yhigh = ss["setup_dict"]["y_range"]*-1, ss["setup_dict"]["y_range"]
-    fig.update_layout(height=ss["setup_dict"]["plot_height"], showlegend=False, yaxis_range=[ylow, yhigh], yaxis2_range=[ylow, yhigh])
+    fig.update_layout(height=ss["setup_dict"]["plot_height"], 
+                      showlegend=False, 
+                      yaxis_range=[ylow, yhigh], 
+                      yaxis2_range=[ylow, yhigh],
+                      margin=dict(l=20, r=20, t=0, b=0))
     #fig.update_layout(hovermode=False)
+    fig.update_traces(marker=dict(size=0.01))
     fig["layout"]["yaxis"]["title"] = "EEG1"
     fig["layout"]["yaxis2"]["title"] = "EEG2"
     return fig
 
-def control_logic():
-    # Control logic
-    col1, col2, col3, col4, col5 = st.columns(5)
-
-    with col1:
-        st.button("first", on_click=change_bucket_idx, args=(len(bins)-1, "first"), use_container_width=True)
-    with col2:
-        streamlit_shortcuts.button("previous", shortcut="ArrowLeft", on_click=dec_bucket_idx)
-    with col3:
-        st.button("find Begin Index", on_click=change_bucket_idx, args=(len(bins)-1, "ts"), use_container_width=True)
-    with col4:
-        streamlit_shortcuts.button("next", shortcut="ArrowRight", on_click=inc_bucket_idx)
-    with col5:
-        st.button("last", on_click=change_bucket_idx, args=(len(bins)-1, "last"), use_container_width=True)
-
-def add_annot():
+def add_annot(trace_select: str):
     #check for correct variables
     if not st.session_state["timestamp1"]:
         st.warning('Begin timestamp not defined.', icon="⚠️")
@@ -173,10 +206,26 @@ def add_annot():
         st.warning('Begin timestamp lager than end timestamp, please correct', icon="⚠️")
         return
     
-    st.session_state["annot_df"].loc[st.session_state["annot_df"].shape[0]] = [st.session_state["timestamp1"], st.session_state["timestamp2"], annotation_dict[st.session_state["valid_annot"]], st.session_state["valid_annot"], neurologist, st.session_state["annot_comment"]]
+    if trace_select == "single":
+        st.session_state["annot_df"].loc[st.session_state["annot_df"].shape[0]] = [st.session_state["timestamp1"], 
+                                                                                   st.session_state["timestamp2"], 
+                                                                                   ss.ts1_curve+1, 
+                                                                                   annotation_dict[st.session_state["valid_annot"]], 
+                                                                                   st.session_state["valid_annot"], 
+                                                                                   neurologist, 
+                                                                                   st.session_state["annot_comment"]]
+    elif trace_select == "both":
+        st.session_state["annot_df"].loc[st.session_state["annot_df"].shape[0]] = [st.session_state["timestamp1"], 
+                                                                                   st.session_state["timestamp2"], 
+                                                                                   3, 
+                                                                                   annotation_dict[st.session_state["valid_annot"]], 
+                                                                                   st.session_state["valid_annot"], 
+                                                                                   neurologist, 
+                                                                                   st.session_state["annot_comment"]]
     
     #reset ss vars
     st.session_state["timestamp1"] = None
+    ss.ts1_curve = None
     st.session_state["timestamp2"] = None
     st.session_state["annot_select"] = "Select annotation type..."
     st.session_state["annot_comment"] = None
@@ -197,23 +246,21 @@ def new_upload():
     st.session_state["bucket_idx"] = 0
     st.session_state["max_bucket"] = False
     st.session_state["bucket_cnt"] = False
-    st.session_state["annot_df"] = pd.DataFrame(data=None, columns=["begin_idx", "end_idx", "annot_id", "annot_txt", "neurologist", "comment"])
+    st.session_state["annot_df"] = pd.DataFrame(data=None, columns=["begin_idx", "end_idx", "trace", "annot_id", "annot_txt", "neurologist", "comment"])
     st.session_state["timestamp1"] = None
     st.session_state["timestamp2"] = None
     st.session_state["view_ts"] = None
 
-annotation_dict = {
-    "Artifact - Missing data": 0,
-    "Artifact - Saturation": 1,
-    "Artifact - Loose channel": 2,
-    "Suppression": 3,
-    "Other Event": 4
-}
+def delete_last_annot():
+    if not st.session_state["annot_df"].empty:
+        st.session_state["annot_df"].drop(st.session_state["annot_df"].index[-1], inplace=True)
+    else:
+        st.warning('No annotations to delete.', icon="⚠️")
 
 #data, bins, bins_dt = generate_random_dataset(4375)
 with st.sidebar:
     st.title('Burst Suppression Annotation Program')
-    neurologist = st.radio("Select annotating neurologist:", ["VM", "JD"], key="neurologist")
+    neurologist = st.radio("Select annotating neurologist:", ["LM", "KE", "JD"], key="neurologist", horizontal=True)
     uploaded_file = st.file_uploader("Choose a EEG recording file", key="uploader", on_change=new_upload)
     st.markdown("""## Setup options:""")
     strip_len = st.slider("Select length of EEG plot in s:", min_value=5, max_value=60, value=30, step=1)
@@ -225,40 +272,30 @@ with st.sidebar:
     st.markdown("""**Missing data**: No Data.  
                 **Saturation**: Extrem values (+- 187.5 uV) due to manipulation, movement etc.  
                 **Loose channel**: Improbably low variance in the signal due to improperly attached electrode or too noisy signal due to high impedance.""")
-    
-    
 
-if uploaded_file is not None:
-    if uploaded_file.name.split(".")[0] == "segments":
-        ss["setup_dict"]["strip_len"] = 15
-    data, bins, bins_dt = prep_data(uploaded_file, ss["setup_dict"]["strip_len"])
-    
-    #Update session state variables
-    if st.session_state["bucket_idx"] == len(bins)-1:
-        st.session_state["max_bucket"] = True
-    else:
-        st.session_state["max_bucket"] = False
-        st.session_state["bucket_cnt"] = len(bins)-1
-    
-    #if slider was used center strip on last view
-    if st.session_state["view_ts"]:
-        change_bucket_idx(len(bins)-1, "view_ts")
-        st.session_state["view_ts"] = None
-
-    # Plot Header
+def header_logic():
     st.subheader(f"""{uploaded_file.name.split('.')[0]}: Plotted {ss["setup_dict"]["strip_len"]}s window ({st.session_state["bucket_idx"]+1}/{len(bins)})""")
     if st.session_state["max_bucket"] == False:
         st.write(f"""from {bins_dt[st.session_state["bucket_idx"]].strftime("%H:%M:%S")} to {bins_dt[st.session_state["bucket_idx"]+1].strftime("%H:%M:%S")}""")
     else:
         st.write(f"""from {bins_dt[st.session_state["bucket_idx"]].strftime("%H:%M:%S")} to end of record""")
 
+def control_logic():
+    # Control logic
+    col1, col2, col3, col4, col5 = st.columns(5)
 
-    fig = build_plot(data, bins, bins_dt)
+    with col1:
+        st.button("first", on_click=change_bucket_idx, args=(len(bins)-1, "first"), use_container_width=True)
+    with col2:
+        st.button("previous", on_click=dec_bucket_idx, use_container_width=True)
+    with col3:
+        st.button("find Begin Index", on_click=change_bucket_idx, args=(len(bins)-1, "ts"), use_container_width=True)
+    with col4:
+        st.button("next", on_click=inc_bucket_idx, use_container_width=True)
+    with col5:
+        st.button("last", on_click=change_bucket_idx, args=(len(bins)-1, "last"), use_container_width=True)    
 
-    clickedPoint = plotly_events(fig, click_event=True, override_height=ss["setup_dict"]["plot_height"])
-    
-    control_logic()
-    
+def annotation_logic():
     col1, col2, col3 = st.columns(3)
     with col1:
         st.text("Begin Index")
@@ -276,7 +313,8 @@ if uploaded_file is not None:
                                 index=0) #, placeholder="Select annotation type...",
         annot_comment = st.text_input("Comment (Other Event) annotation:", key="annot_comment")
     with col3:
-        st.button("Add Annotation", key="add_annot_bn", on_click=add_annot, use_container_width=True)
+        st.button("Add **Single Trace** Annotation", key="add_annot_single_bn", on_click=add_annot, use_container_width=True, args=("single",))
+        st.button("Add **Both Traces** Annotation", key="add_annot_both_bn", on_click=add_annot, use_container_width=True, args=("both",))
         #streamlit_shortcuts.button("add Annotation", shortcut="Enter", on_click=add_annot)
 
     with st.expander("Annotations"):
@@ -284,43 +322,65 @@ if uploaded_file is not None:
         with col1:
             st.write(st.session_state["annot_df"])
         with col2:
-            st.download_button("Download annotations", data=st.session_state["annot_df"].to_csv(), 
+            st.download_button("Download Annotations", data=st.session_state["annot_df"].to_csv(), 
                            file_name=f"{uploaded_file.name.split('.')[0]}_annot_{neurologist}.csv", use_container_width=True)
+            st.button("Delete last Annotation", on_click=delete_last_annot, use_container_width=True)
 
-    #if st.checkbox("Show bins"):
-    #    st.write(bins)
-    #    st.write(bins_dt)
-
-    #if st.checkbox('Show raw data'):
-    #    st.subheader('Raw data')
-    #    st.write(data)
-
+if uploaded_file is not None:
+    if uploaded_file.name.split(".")[0] == "segments":
+        ss["setup_dict"]["strip_len"] = 15
+    data, bins, bins_dt = prep_data(uploaded_file, ss["setup_dict"]["strip_len"])
+    
+    #Update session state variables
+    if st.session_state["bucket_idx"] == len(bins)-1:
+        st.session_state["max_bucket"] = True
+    else:
+        st.session_state["max_bucket"] = False
+        st.session_state["bucket_cnt"] = len(bins)-1
+    #if slider was used center strip on last view
+    if st.session_state["view_ts"]:
+        change_bucket_idx(len(bins)-1, "view_ts")
+        st.session_state["view_ts"] = None
 
     
-    if clickedPoint:
-        ts = dt.strptime(clickedPoint[0]["x"], "%Y-%m-%d %H:%M:%S.%f")
+    # Plot Header
+    header_logic()
+    PlotPlaceholder = st.empty()
+    OverviewPlaceholder = st.empty()
+    control_logic()
+    annotation_logic()
+
+
+    fig = build_plot(data, bins, bins_dt)
+
+    def point_clicked():
+        point = ss.click_data["selection"]["points"][0]
+        x = point["x"]
+        curve = point["curve_number"]
+        ts = dt.strptime(x, "%Y-%m-%d %H:%M:%S.%f")
         valid_annot = get_annot()
         st.session_state["valid_annot"] = valid_annot
         if not st.session_state["timestamp1"]: #first click in plot
             st.session_state["timestamp1"] = ts
-            st.rerun()
+            ss.ts1_curve = curve
+            
         elif st.session_state["timestamp1"] and not valid_annot:
             st.session_state["timestamp1"] = ts
-            st.rerun()
+            ss.ts1_curve = curve
+        
         elif st.session_state["timestamp1"] and valid_annot and not st.session_state["timestamp2"]: #subsequent clicks in plot != previous point
             st.session_state["timestamp2"] = ts
-            st.rerun()
+        
         elif st.session_state["timestamp1"] and valid_annot and st.session_state["timestamp2"]:
             st.session_state["timestamp2"] = ts
-            st.rerun()
+
+    with PlotPlaceholder.container():
+        st.plotly_chart(fig, use_container_width=True, on_select=point_clicked, key="click_data", config = {'displayModeBar': False} )
+    
+    with OverviewPlaceholder.container():
+        st.plotly_chart(build_overview(data, bins), use_container_width=True, key="overview", config = {'staticPlot': True} )
+    
+
 else:
     st.write("Please upload file to start annotating!")
 
-#ss
-
-# Delete a single key-value pair
-#del st.session_state[key]
-
-# Delete all the items in Session state
-#for key in st.session_state.keys():
-#    del st.session_state[key]
